10ed3043b7d0d555bba3909257052f5c558cb136
Sri Nath <sri.developer731@gmail.com>
2018-12-09 23:06:22 -0500

    Complete capybara product page testing

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details page", type: :feature do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit '/products/1'
+
+    # DEBUG / VERIFY
+    # save_screenshot
+
+    # VERIFY
+    expect(page).to have_content 'Name', 'Description'
+  end
+
+end
